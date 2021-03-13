2c90f6a350ec9e4bde185be7af8a37149de07253
ALIAKSEI ZANKA <goro10112001@gmail.com>
2021-03-13 18:56:23 +0300

    added function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0) {
+        n = -n; 
+    };
+    let str = String(n);
+    str = str.split('').reverse().join('');
+    n = Number(str);
+    if (n < 0) {
+        n = n * -1;
+    }
+    return n;  
 }
